184906948df2c54cbbdf400898fac60cdc776fb8
Ali Samad <aliussamad2004@gmail.com>
2025-06-04 02:18:05 +0530

    Updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-## End to End Machine Learning Project
+# End to End Machine Learning Project
+
+## Overview
+This project is an end-to-end machine learning solution.
+
+## Setup
+1. Clone the repository.
+2. Install dependencies:
+   ```
+   pip install -r requirements.txt
+   ```
+
+## Usage
+Instructions on how to use the project will be added here.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,6 +23,22 @@ class DataIngestion:
             df = pd.read_csv("notebooks\data\stud1.csv")
             logging.info("Read the dataset as dataframe")
 
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.train_data_path,index=False, header = True)
             
-        except:
-            pass
+            logging.info("Train test Split initiated")
+            train_set,test_set  = train_test_split(df,test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index = False, header = True)
+            train_set.to_csv(self.ingestion_config.test_data_path,index = False, header = True)
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config,test_data_path
+            )
+        except Exception as e:
+            raise CustomException  (e,sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.intiate_data_ingestion()
